c11b979b7ce8717fa2ab26353ac23124ab0bd197
Edwin Rojas <enrojass04@gmail.com>
2024-03-08 16:59:17 -0500

    actualizacion pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'Reto-1-de-Programacion/'
+  base: 'https://enrojass04.github.io/Reto1programacion/'
 })
